f71b02d77ac7b74728f58b5b5b4df7abbf75338b
yasaino <yasainoonabe@outlook.jp>
2023-08-31 11:20:40 +0900

    直近48時間以内の投稿が表示されるようにしました

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,7 +10,7 @@ class StaticPagesController < ApplicationController
   def new
     if logged_in?
       @micropost  = current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
+      @feed_items = current_user.feed.where("created_at >= ?", 48.hours.ago).paginate(page: params[:page],per_page: 10)
     end
   end
 
