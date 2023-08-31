5d616501505254193e491453d671e209b5c6f049
kiji-haru <kijiharu3112@outlook.jp>
2023-09-01 07:43:10 +0900

    テストを変更

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,13 +1,14 @@
 require "test_helper"
+require_relative "../../utility/add_query_params.rb"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", add_query_params(root_path), count: 2
+    assert_select "a[href=?]", add_query_params(help_path)
+    assert_select "a[href=?]", add_query_params(about_path)
+    assert_select "a[href=?]", add_query_params(contact_path)
   end
 end
