b754593f517d5c4b45a24f64a5f5730207963c73
mametaro99 <bearsmichimato99@gmail.com>
2023-09-01 11:21:33 +0900

    変更

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,14 +2,14 @@ class StaticPagesController < ApplicationController
 
   def home
     if logged_in?
-      @micropost = current_user.microposts.build(deleted_flag: false)
+      @micropost = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
 
   def deleted_post_index
     if logged_in?
-      @microposts = current_user.microposts.build(deleted_flag: true)
+      @microposts = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
