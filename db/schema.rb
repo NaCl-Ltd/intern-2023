dc4caf2a3f4fb89891953af89fcc43368d5555d5
Suzune2741 <kannra1174@gmail.com>
2023-09-01 13:26:22 +0900

    DBに出身地を、設定画面に出身地入力画面の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_30_104335) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
+ActiveRecord::Schema[7.0].define(version: 2023_09_01_041026) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -44,9 +41,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_104335) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
-    t.bigint "user_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "fixed"
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
@@ -75,6 +73,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_104335) do
     t.string "reset_digest"
     t.datetime "reset_sent_at"
     t.text "introduction"
+    t.text "birthplace"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
