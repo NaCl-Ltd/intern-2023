d037e05f34e7aba87200aea15d901665a9b6882f
Suzune2741 <kannra1174@gmail.com>
2023-09-05 14:06:57 +0900

    typo修正

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy] do 
     member do 
-      get :show_user,fixed
+      get :show_user,:fixed
     end
   end
   resources :relationships,       only: [:create, :destroy]
