d748c863c6bcaa8ce6608d0f69334ae21d30d0cd
Suzune2741 <kannra1174@gmail.com>
2023-09-05 13:32:27 +0900

    文字が被らないようにcssに修正を加えました

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   validates_length_of :birthplace, maximum: 10
+  validates_length_of :introduction, maximum: 100
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
