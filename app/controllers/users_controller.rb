a7970ccf7b66343f8b69f3237ef74650885048d1
Suzune2741 <kannra1174@gmail.com>
2023-09-05 14:37:39 +0900

    キーワード検索で入力が空白の場合間違った検索結果になる不具合の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[index show create edit update destroy following followers]
 
   def index
+
     @users = User.paginate(page: params[:page])
     if params[:select] == "ユーザー検索"
       search_term = "%#{params[:data]}%"
@@ -14,8 +15,11 @@ class UsersController < ApplicationController
                         or(@users.where('email LIKE ?', search_term)).
                         or(@users.where('birthplace LIKE ?', search_term))
     elsif params[:select] == "キーワード検索"
-      search_term = "%#{User.sanitize_sql_like(params[:data])}%"
-      @users = @users.where('introduction LIKE ?', search_term)
+      ##入力が空白だったら何もしない
+      if params[:data] != ""
+        search_term = "%#{User.sanitize_sql_like(params[:data])}%"
+        @users = @users.where('introduction LIKE ?', search_term)
+      end
     end
   end
 
