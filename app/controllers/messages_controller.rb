ea14c8f3ecb1976e77395482c2aeea1089dfba96
mametaro99 <bearsmichimato99@gmail.com>
2023-09-06 22:30:37 +0900

    メッセージの入力機能を作成しました。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,25 @@
 class MessagesController < ApplicationController
+
   def index
     @micropost = Micropost.find(params[:micropost_id])
     @messages = Message.where(micropost_id: params[:micropost_id])
+    @message = Message.new
   end
 
   def new
-
+    
   end
 
   def edit
   end
 
   def create
+    @message = Message.new(user_id:current_user.id, micropost_id:params[:micropost_id], content: params[:message][:content])
+    
+    if @message.save
+      flash[:success] = "messages created!"
+    end
+    redirect_to request.referrer
   end
 
   def update
@@ -19,4 +27,7 @@ class MessagesController < ApplicationController
 
   def destroy
   end
+
+  private
+
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,6 +7,7 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @micropost = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
+      @message = Message.new
     end
   end
 
