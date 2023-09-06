0810f34dbd1eb03b4108db2e8ad26d41e1a60a78
mametaro99 <bearsmichimato99@gmail.com>
2023-09-06 11:38:24 +0900

    seed.rbの変更

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_09_05_042538) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -40,8 +43,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_042538) do
   end
 
   create_table "bads", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "micropost_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "micropost_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["micropost_id"], name: "index_bads_on_micropost_id"
@@ -49,8 +52,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_042538) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "micropost_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "micropost_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["micropost_id"], name: "index_likes_on_micropost_id"
@@ -59,9 +62,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_042538) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "deleted_flag", default: false
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
